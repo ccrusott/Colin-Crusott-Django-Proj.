652057d13fe1d57e04fc3ab233013b8b15d39cb1
ccrusott <ccrusott@gmail.com>
2022-09-29 00:31:32 +0900

    complete all models, make migrations

--- Django_Delights/inventory/models.py
+++ Django_Delights/inventory/models.py
@@ -1,23 +1,29 @@
+from tkinter import CASCADE
 from django.db import models
-
+import datetime
 # Create your models here.
-class Ingredient(models.model):
+class MenuItem(models.Model):
+    menu_item_name = models.CharField(max_length=30)
+    item_price = models.FloatField(max_length=5)
+
+    def __str__(self):
+        return self.menu_item_name + " costs " + str(self.item_price)
+class Ingredient(models.Model):
     name = models.CharField(max_length=30)
     quantity = models.IntegerField(default=0)
     price_per_unit = models.FloatField(default=0.0)
-
-class MenuItem(models.model):
-    grilled_cheese = "GC"
-    cheeseburger = "CB"
-    hotdog = "HD"
-    menu_choices = [
-        (grilled_cheese, "Grilled Cheese"), 
-        (cheeseburger, "Cheeseburger"),
-        (hotdog, "HotDog"),
-    ]
-    menu_item = CharField(max_length=2, choices=menu_choices, blank=True)
-class RecipeRequirements(models.model):
-    pass
-
-class Purchase(models.model):
-    pass
+    unit = models.CharField(max_length=30, default="Units")
+    def __str__(self):
+        return "There are " + str(self.quantity) + " " + self.unit + "s of " +self.name + " available. They cost " + str(self.price_per_unit) + " per " + self.unit
+class RecipeRequirements(models.Model):
+    ingredients = models.ManyToManyField(Ingredient)
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity_needed = models.FloatField(default=0.0)
+    
+    def __str__(self):
+        return "You need " + str(self.quantity_needed) + " of " + self.ingredients + " to make " + self.menu_item
+class Purchase(models.Model):
+    purchased_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    def __str__(self):
+        return self.purchased_item + " was purchased on " + str(self.timestamp)
